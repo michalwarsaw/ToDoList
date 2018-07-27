e8779f9109adf6b3ae8d9bb17b25af831c02e1f2
Michal Wlodarczyk <polskito@gmail.com>
2018-07-27 11:12:17 +0200

    Update 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: "./js/index.jsx",
-    output: { filename: "js/out.js" },
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'js'),
+    filename: 'out.js'},
     watch: true,
-    module: {
-        loaders: [{
-            test: /\.jsx$/,
-            exclude: /node_modules/,
-            loader: 'babel',
-            query: { presets: ['es2015', 'stage-2', 'react'] }
-        }]
-    }
+  module: {
+    rules: [
+      { 
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: { 
+            presets: ['env', 'stage-2', 'react']
+          }
+        }
+      },
+      { 
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|svg|gif|webp)/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  devServer: {
+    historyApiFallback: true
+  }
 };
